6ca3d51e05783cb60be03443aebb33fdf61ee3d6
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-03-13 16:00:41 -0700

    add chromedriver dependency

--- DL-File-Deleter/index.js
+++ DL-File-Deleter/index.js
@@ -1,40 +1,44 @@
-const {Builder, By, until} = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+require('chromedriver');
 
-let driver = new Builder()
-    .forBrowser('chrome')
+let chromeCapabilities = webdriver.Capabilities.chrome();
+chromeCapabilities.set('chromeOptions', {'args':['--disable-infobars']}); // disable popup
+
+let driver = new webdriver.Builder()
+    .forBrowser('chrome').withCapabilities(chromeCapabilities)
     .build();
 
-driver.get('moodleSiteToDelete'); //put url here
+driver.get('https://www.google.com'); //put url here
 
-//Log into the website
-driver.findElement(By.linkText('Log in')).click();
-driver.findElement(By.name('username')).sendKeys('username123');
-driver.findElement(By.name('password')).sendKeys('notMyPassword');
-driver.findElement(By.name('submit')).click();
+// //Log into the website
+// driver.findElement(By.linkText('Log in')).click();
+// driver.findElement(By.name('username')).sendKeys('username123');
+// driver.findElement(By.name('password')).sendKeys('notMyPassword');
+// driver.findElement(By.name('submit')).click();
 
-driver.get('moodleSiteToDelete'); //redirect back to the website (moodle)
-driver.executeScript("scroll(0,1500)");
-driver.findElement(By.linkText('Legacy course files')).click();
-driver.executeScript("scroll(0,8000)"); //scroll to bottom of page
-driver.findElement(By.css('input[value="Edit legacy course files"]')).click();
-driver.sleep(8000); //wait for widget to load
+// driver.get('moodleSiteToDelete'); //redirect back to the website (moodle)
+// driver.executeScript("scroll(0,1500)");
+// driver.findElement(By.linkText('Legacy course files')).click();
+// driver.executeScript("scroll(0,8000)"); //scroll to bottom of page
+// driver.findElement(By.css('input[value="Edit legacy course files"]')).click();
+// driver.sleep(8000); //wait for widget to load
 
 
-for(let i = 0; i < 23; i++){ //delete 21 folders
+// for(let i = 0; i < 23; i++){ //delete 21 folders
 
-   driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child a.fp-contextmenu')).click(); //for folders
-   deleteContent();
-}
+//    driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child a.fp-contextmenu')).click(); //for folders
+//    deleteContent();
+// }
 
-for(let i = 0; i < 24; i++){ //delete 10 files
+// for(let i = 0; i < 24; i++){ //delete 10 files
 
-   driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child')).click(); //for files
-   deleteContent();
-}
+//    driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child')).click(); //for files
+//    deleteContent();
+// }
 
-function deleteContent(){
-    driver.findElement(By.css('.fp-file-delete')).click();
-    driver.findElement(By.css('.fp-dlg-butconfirm.btn-primary.btn')).click(); //confirm delete button
-    driver.sleep(5000); //wait for widget to load
-}
+// function deleteContent(){
+//     driver.findElement(By.css('.fp-file-delete')).click();
+//     driver.findElement(By.css('.fp-dlg-butconfirm.btn-primary.btn')).click(); //confirm delete button
+//     driver.sleep(5000); //wait for widget to load
+// }
 // driver.quit();
