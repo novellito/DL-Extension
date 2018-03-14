4229033de003f748057660192e670db6aa52d8a8
Christian Trinidad <christian.trinidad.501@my.csun.edu>
2018-03-13 23:19:51 -0700

    fix crashing problem

--- DL-File-Deleter/electron-workspace/main.js
+++ DL-File-Deleter/electron-workspace/main.js
@@ -18,10 +18,9 @@ exports.handleForm = function handleForm(targetWindow, data) {
   targetWindow.webContents.send('form-received', "we got it");
 };
 
-
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 770, resizable:false});
+  mainWindow = new BrowserWindow({width: 800, height: 870, resizable:false});
   // mainWindow.setMenu(null);
 
   // and load the index.html of the app.
@@ -30,9 +29,6 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }));
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
--- DL-File-Deleter/electron-workspace/renderer.js
+++ DL-File-Deleter/electron-workspace/renderer.js
@@ -10,11 +10,11 @@ let chromeCapabilities = webdriver.Capabilities.chrome();
 chromeCapabilities.set('chromeOptions', {'args':['--disable-infobars']}); // disable popup
 
 const submitFormButton = document.querySelector("#ipcForm2");
-const responseParagraph = document.getElementById('response')
+const responseParagraph = document.getElementById('response');
 
-submitFormButton.addEventListener("submit", function(event) {
+submitFormButton.addEventListener("submit",  function(event) {
         event.preventDefault();   // stop the form from submitting
-
+        
         if (document.getElementById("folders").value && document.getElementById("files").value) {
             handleForm(currentWindow, {status:"err", msg:"<b>Please enter one or the other (not both)</b>"}); // let user know to enter only one
         } else if (document.getElementById("files").value) { // do files
@@ -34,22 +34,16 @@ submitFormButton.addEventListener("submit", function(event) {
             }
             handleForm(currentWindow, data);            
         } else { //user didnt enter anything 
-            responseParagraph.innerHTML = "<b>Please fill in either # files or # folders</b>"
+            responseParagraph.innerHTML = "<b>Please fill in either # files or # folders</b>";
         }
 
 });
 
 ipcRenderer.on('send-err', function(event, args){
-    responseParagraph.innerHTML = args
+    responseParagraph.innerHTML = args;
 });
 
 ipcRenderer.on('send-files', function(event, args){
-    console.log(args);
-    // let driver = new webdriver.Builder()
-    //     .forBrowser('chrome').withCapabilities(chromeCapabilities)
-    //     .build();
-
-    // driver.get(args.courseURL); 
     runDriver(args);
 });
 
@@ -57,53 +51,41 @@ ipcRenderer.on('send-folders', function(event, args){
     runDriver(args);
 });
 
-function runDriver(args) {
-    console.log(args.courseURL);
+async function runDriver(args) {
     let driver = new webdriver.Builder()
         .forBrowser('chrome').withCapabilities(chromeCapabilities)
         .build();
 
-    driver.get(args.courseURL); 
-    
-    
-    //Log into the website
-    driver.findElement(By.linkText('Log in')).click();
-    driver.sleep(2000);     
-    driver.findElement(By.name('username')).sendKeys(args.username);
-    driver.findElement(By.name('password')).sendKeys(args.password);
-    driver.findElement(By.name('submit')).click();
-    
-    driver.get(args.courseURL); //redirect back to the website (moodle)
-    
-    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.linkText('Legacy course files'))); // scroll to link
-    driver.findElement(By.linkText('Legacy course files')).click();
-    
-    driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.css('input[value="Edit legacy course files"]'))); // scroll to bottom of page
-    driver.findElement(By.css('input[value="Edit legacy course files"]')).click();
-    
-    driver.sleep(8000); //wait for widget to load
+    await driver.get('https://auth.csun.edu/cas/login?service=https%3A%2F%2Fmoodle.csun.edu%2Flogin%2Findex.php');   
+    await driver.findElement(By.xpath('//*[@id="username"]')).sendKeys(args.username);
+    await driver.findElement(By.xpath('//*[@id="password"]')).sendKeys(args.password);
+    await driver.findElement(By.xpath('//*[@id="fm1"]/fieldset/ol/li[3]/input[4]')).click();
+    await driver.get(args.courseURL);
+    await driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.linkText('Legacy course files'))); // scroll to link
+    await driver.findElement(By.linkText('Legacy course files')).click();
+    await driver.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.css('input[value="Edit legacy course files"]'))); // scroll to bottom of page
+    await driver.findElement(By.css('input[value="Edit legacy course files"]')).click();
+    await driver.sleep(8000);
 
-    // if(args.files) {
-    //     for(let i = 0; i < args.files; i++){
-    //         driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child')).click(); 
-    //         driver.findElement(By.css('.fp-file-delete')).click();
-    //         driver.findElement(By.css('.fp-dlg-butconfirm.btn-primary.btn')).click(); //confirm delete button
-    //         driver.sleep(5000); //wait for widget to load
-    //     }
-    //     driver.quit();
-    // } else {
-    //     for(let i = 0; i < args.folders; i++){ 
-    //         driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child a.fp-contextmenu')).click(); //for folders
-    //         driver.findElement(By.css('.fp-file-delete')).click();
-    //         driver.findElement(By.css('.fp-dlg-butconfirm.btn-primary.btn')).click(); //confirm delete button
-    //         driver.sleep(5000); //wait for widget to load
-    //      }
-    //     driver.quit();
-    // }
+    if(args.files) {
+    
+         for(let i = 0; i < args.files; i++) {
+            await driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child')).click(); 
+            await driver.findElement(By.css('.fp-file-delete')).click();
+            await driver.findElement(By.css('.fp-dlg-butconfirm.btn-primary.btn')).click(); //confirm delete button
+            await driver.sleep(5000); //wait for widget to load
+        }
+        await driver.sleep(120000); // wait 2 minutes before closing browser
+        await driver.quit();
+    } else {
+        for(let i = 0; i < args.folders; i++) { 
+            await driver.findElement(By.css('div.fp-file.fp-hascontextmenu:first-child a.fp-contextmenu')).click()
+            await driver.findElement(By.css('.fp-file-delete')).click();
+            await driver.findElement(By.css('.fp-dlg-butconfirm.btn-primary.btn')).click();
+            await driver.sleep(5000); //wait for widget to load
+        }
+        await driver.sleep(120000); // wait 2 minutes before closing browser
+        await driver.quit();
+    }
 
 }
-
-
-
-
-
